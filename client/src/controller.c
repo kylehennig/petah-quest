1d6dda0e1374aec6fb48b8018eb338175526228a
Peter Elliott <p.elliott332@gmail.com>
2017-11-18 14:28:59 -0700

    updated controller and draw

--- client/src/controller.c
+++ client/src/controller.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include "net.h"
 #include "draw.h"
+#include <ncurses.h>
 
 
 void server_controller(int sockfd, struct map *map, struct entity_list *elist) {
@@ -12,17 +13,24 @@ void server_controller(int sockfd, struct map *map, struct entity_list *elist) {
             uint32_t x, y;
             get_flash(sockfd, &x, &y);
             do_flash();
+            refresh();
             break;
         case NEW:
             uint32_t id, x, y;
             char ch;
             uint8_t colour;
+
+            dedraw_entity(&elist->list[id]);
+
             get_new(int sockfd, &id, &x, &y, &ch, &colour);
             elist->list[id]->x = x;
             elist->list[id]->y = y;
             elist->list[id]->ch = ch;
             elist->list[id]->colur = colour;
-            redraw_entity(elist, &id);
+
+            draw_entity(&elist->list[id]);
+            refresh();
+            break;
 
     }
 }
--- client/src/draw.h
+++ client/src/draw.h
@@ -6,7 +6,8 @@
 void draw_map_at(struct entity_list *list, uint32_t x, uint32_t y);
 void draw_entities_screen(struct entity_list *list, uint32_t x, uint32_t y);
 
-void redraw_entity(struct entity_list *list, uint32_t id);
+void dedraw_entity(struct entity *ent);
+void draw_entity(struct entity *ent);
 
 void do_flash(uint32_t x, uint32_t y);
 

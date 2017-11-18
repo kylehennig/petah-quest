d3866993328849099ec8731fb00e5a1f451d315d
Alex Rostron <alex.rostron24@gmail.com>
2017-11-18 14:14:28 -0700

    Make get.c work

--- client/src/get.c
+++ client/src/get.c
@@ -1,14 +1,42 @@
 #include <unistd.h>
 
-
+//reads the first sizeof() bytes into variables left to right
 void get_flash(int sockfd, uint32_t *x, uint32_t *y){
-	read(sockfd,x,sizeof(x)); //should read x from sockfd
-	read(sockfd,x,sizeof(y));
+	read(sockfd,x,sizeof(uint32_t)); //should read x from sockfd
+	read(sockfd,y,sizeof(uint32_t));
 }
 
-void get_new(int sockfd, uint32_t *id, uint32_t *x, uint32_t *y){}
-void get_delete(int sockfd, uint32_t *id){}
-void get_text(int sockfd, size_t len, char **str){}
-void get_move(int sockfd, uint32_t *id, uint32_t *x, uint32_t *y){}
-void get_update(int sockfd, uint32_t *id, char *ch, uint8_t colour){}
-void get_health(int sockfd, uint8_t health){}
+void get_new(int sockfd, uint32_t *id, uint32_t *x, uint32_t *y){
+	read(sockfd,id,sizeof(uint32_t));
+	read(sockfd,x,sizeof(uint32_t));
+	read(sockfd,y,sizeof(uint32_t));
+
+}
+
+void get_delete(int sockfd, uint32_t *id){
+	read(sockfd,id,sizeof(uint32_t));
+}
+
+void get_text(int sockfd, size_t len, char **str){
+	read(sockfd,len,sizeof(size_t));
+	read(sock,str,sizeof(&str));  //LIKELY TO FAIL
+
+}
+
+void get_move(int sockfd, uint32_t *id, uint32_t *x, uint32_t *y){
+	read(sockfd,id,sizeof(uint32_t));
+	read(sockfd,x,sizeof(uint32_t));
+	read(sockfd,y,sizeof(uint32_t));
+}
+
+void get_update(int sockfd, uint32_t *id, char *ch, uint8_t colour){
+	read(sockfd,id,sizeof(uint32_t));
+	read(sockfd,ch,sizeof(char));
+	read(sockfd,colout,sizeof(uint8_t));
+
+}
+
+void get_health(int sockfd, uint8_t health){
+	read(sockfd,health,sizeof(uint8_t));
+
+}

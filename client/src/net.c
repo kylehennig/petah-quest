8de9f72d7d2b63dc09eb57294f23820b14f7d396
Peter Elliott <p.elliott332@gmail.com>
2017-11-18 14:14:29 -0700

    added makefile

--- client/src/net.c
+++ client/src/net.c
@@ -1,18 +1,19 @@
 #include "net.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 
 int handshake(struct map *map, char ch, int sockfd) {
-    write(sockfd, ch, sizeof(char));
+    write(sockfd, &ch, sizeof(char));
     if (read(sockfd, &map->width, sizeof(uint32_t)) == 0) {
-        exit();
+        exit(1);
     }
     if (read(sockfd, &map->height, sizeof(uint32_t)) == 0) {
-        exit();
+        exit(1);
     }
 
     map->map = malloc(map->height*map->width);
     if (read(sockfd, map->map, map->height*map->width) == 0) {
-        exit();
+        exit(1);
     }
 }

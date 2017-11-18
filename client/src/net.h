5b4199304afdd05149042c83957db3033a8c1bd4
Peter Elliott <p.elliott332@gmail.com>
2017-11-18 16:31:59 -0700

    merege get.c into net.c and fixed errors

--- client/src/net.h
+++ client/src/net.h
@@ -18,7 +18,7 @@ void get_delete(int sockfd, uint32_t *id);
 void get_text(int sockfd, size_t *len, char **str);
 void get_move(int sockfd, uint32_t *id, uint32_t *x, uint32_t *y);
 void get_update(int sockfd, uint32_t *id, char *ch, uint8_t *colour);
-void get_health(int sockfd, uint8_t health);
+void get_health(int sockfd, uint8_t *health);
 
 
 

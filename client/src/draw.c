e38573af16e7d250327260572235849769de173e
Alex Rostron <alex.rostron24@gmail.com>
2017-11-18 16:22:28 -0700

    Make Draw

---
+++ client/src/draw.c
@@ -0,0 +1,33 @@
+
+
+//chunks
+void draw_map_at(struct entity_list *list, uint32_t x, uint32_t y){
+
+
+}
+
+//draws a single character to the screen
+//takes in coords from global map as well as where to draw the character to the screen
+void draw_character(struct map *map, uint32_t screenDestx, uint32_t screenDesty, uint32_t screenDestx, uint32_t mapX, uint32_t mapY){
+	//get character from map
+	//map.width
+	//map.height
+
+	uint32_t dest;
+
+	map.map[dest]
+
+	//switch on character
+
+}
+
+
+void draw_entities_screen(struct entity_list *list, uint32_t x, uint32_t y){
+
+
+}
+
+void redraw_entity(struct entity_list *list, uint32_t id){
+
+
+}
--- client/src/draw.h
+++ client/src/draw.h
@@ -5,7 +5,6 @@
 
 void draw_map_at(struct entity_list *list, uint32_t x, uint32_t y);
 void draw_entities_screen(struct entity_list *list, uint32_t x, uint32_t y);
-
 void redraw_entity(struct entity_list *list, uint32_t id);
 
 #endif

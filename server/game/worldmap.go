ef92a33d5fd6348a87f9b34473b83a698030aa80
Jacob Reckhard <jacobreckhard@gmail.com>
2017-11-18 17:32:59 -0700

    sorry

--- server/game/worldmap.go
+++ server/game/worldmap.go
@@ -1,12 +1,10 @@
 package game
 
-
 type WorldMap struct {
-	width int
+	width  int
 	height int
-	tiles []byte
+	tiles  []byte
 }
-
 
 func (wMap WorldMap) ToBytes() []byte {
 	bytes := make([]byte, 0)
@@ -22,14 +20,14 @@ func (wMap WorldMap) ToBytes() []byte {
 	return bytes
 }
 
-func GetMap() WorldMap{
+func GetMap() WorldMap {
 	return loadMap()
 }
 func intToBytes(n int) []byte {
 	bytes := make([]byte, 4)
-	bytes[3] = (byte) ((n >> 24) & 0xFF)
-	bytes[2] = (byte) ((n >> 16) & 0xFF)
-	bytes[1] = (byte) ((n >> 8) & 0xFF)
-	bytes[0] = (byte) (n & 0xFF)
+	bytes[3] = (byte)((n >> 24) & 0xFF)
+	bytes[2] = (byte)((n >> 16) & 0xFF)
+	bytes[1] = (byte)((n >> 8) & 0xFF)
+	bytes[0] = (byte)(n & 0xFF)
 	return bytes
 }

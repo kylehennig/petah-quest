7b14dada5582dbbd3aa2db2d2819cfa3a37f168d
Peter Elliott <p.elliott332@gmail.com>
2017-11-18 23:31:36 -0700

    added hud kindof

---
+++ client/src/hud.c
@@ -0,0 +1,12 @@
+#include "hud.h"
+#include <ncurses.h>
+
+
+const char *weapons = ")}/-*!@O"
+
+void draw_weapon_sel(uint8_t weapon) {
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+
+    
+}

8de34bd1852bf5325d4fa0ad334d8a50b3462396
Jacob Reckhard <jacobreckhard@gmail.com>
2017-11-19 08:05:45 -0700

    i hate goblins!!!!!!!!!!!!

--- client/src/draw.h
+++ client/src/draw.h
@@ -3,7 +3,7 @@
 
 #include "entity.h"
 #define CHUNK_WIDTH 80
-#define CHUNK_HEIGHT 24
+#define CHUNK_HEIGHT 23
 
 void draw_map_at(struct map *map, uint32_t x, uint32_t y);
 void draw_entities_screen(struct entity_list *list, struct entity *player);
